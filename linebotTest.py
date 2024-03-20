c0619c41fef36429d01e733359998b449ca42a26
日廣 <kevintseng0810@gmail.com>
2024-03-20 17:59:12 +0800

    Update linebotTest.py

--- linebotTest.py
+++ linebotTest.py
@@ -24,4 +24,4 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
